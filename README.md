4599df48074ce5eb9f7996420ae9b451b0c0e8c4
Jamal077011 <Jamal@sketch-tech.com>
2024-01-18 04:19:40 -0500

    Pushing the Deoployed Server

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>installation</h1>
+<h6>composer install</h6>
+<h6>npm install</h6>
+<h6>php artisan migrate --seed</h6>
+<h6>php artisan serve</h6>
+<h6>npm run dev</h6>
---
+++ app/Http/Controllers/Frontend/Requests/RequestController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Requests;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Form;
+
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $requests=  Form::all() ;
+
+        return view('frontend.dashboard.pages.requests.index',compact('requests')) ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
