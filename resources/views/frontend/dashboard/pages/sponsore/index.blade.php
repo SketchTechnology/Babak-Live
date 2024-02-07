a01982c24f3b079b1d7b38d95c5ab3a83ae9aef8
hebatallah10 <heba@sketch-tech.com>
2024-02-07 08:53:32 +0200

    margin company view

--- resources/views/frontend/dashboard/pages/sponsore/index.blade.php
+++ resources/views/frontend/dashboard/pages/sponsore/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
+<div class="arabic p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
     <div class="w-full mb-1">
         <div class="mb-4">
             <nav class="flex mb-5" aria-label="Breadcrumb">
@@ -30,7 +30,7 @@
 
     </div>
 </div>
-<div class="flex flex-col">
+<div class="arabic flex flex-col">
     <div class="overflow-x-auto">
         <div class="inline-block min-w-full align-middle">
             <div class="overflow-hidden shadow">
